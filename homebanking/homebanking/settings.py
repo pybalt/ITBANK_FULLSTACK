3eb4ec10171f05eed9531de0322ba3e8d2049fcf
LautyT02 <lautyt02@gmail.com>
2022-08-09 15:47:34 -0300

    Añadir Base de Datos

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -78,7 +78,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'itbank': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'itbank.db',
+    },
 }
 
 
