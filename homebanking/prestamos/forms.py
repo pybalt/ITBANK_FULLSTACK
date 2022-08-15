1bc700ad23e9135b7da83fb8e81560555142f0a6
LautyT02 <lautyt02@gmail.com>
2022-08-15 11:58:58 -0300

    Formulario Prestamos
    Se agregó el menu desplegable al formulario

--- homebanking/prestamos/forms.py
+++ homebanking/prestamos/forms.py
@@ -1,1 +1,14 @@
+from email.policy import default
 from django import forms
+class SolicitudPrestamoForm(forms.Form):
+    name = forms.CharField(label= 'Nombres', required=True)
+    surname = forms.CharField(label= 'Apellidos', required=True)
+    dni= forms.IntegerField(label= 'DNI', required= True)
+    startDate = forms.DateField(label = 'Fecha de Inicio', required=True)
+    loan_type = forms.ChoiceField(label = 'Tipo de Préstamo', required=True, 
+    choices= [
+        ('HIPOTECARIO', 'Hipotecario'),
+        ('PERSONAL', 'Personal'),
+        ('PRENDARIO', 'Prendario'),
+    ], default= 'PERSONAL')
+    amount = forms.IntegerField(label = 'Monto del Préstamo', required=True) 
