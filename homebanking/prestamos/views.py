60a53bf2791e722211b9321d63d163f45b27e262
LautyT02 <lautyt02@gmail.com>
2022-08-15 12:00:19 -0300

    Formulario Prestamos
    Se modificó el view de prestamos

--- homebanking/prestamos/views.py
+++ homebanking/prestamos/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import SolicitudPrestamoForm
+from .models import Prestamo
 def prestamos(request):
-    return render(request, "prestamos/prestamos.html")
+    form_prestamo = SolicitudPrestamoForm()
+    if request.method == "POST":
+        if form_prestamo.is_valid():
+            name = request.POST.get('Nombres','')
+            surname = request.POST.get('Apellidos','')
+        return redirect(reverse('prestamos')+ "?ok")    
+    return render(request, "prestamos/prestamos.html",
+    {'form':form_prestamo})
 # Create your views here.
