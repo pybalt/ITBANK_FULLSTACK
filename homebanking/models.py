0643e95948c96aaccafdb792a61691bed1ebf1c7
Luis <sergioluispagano@gmail.com>
2022-08-16 17:20:03 -0300

    I tried so hard, and got so far, but in the end

--- homebanking/models.py
+++ homebanking/models.py
@@ -1,3 +1,4 @@
+
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
@@ -6,6 +7,46 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import User
+
+class Cards(models.Model):
+    customer_id = models.AutoField()
+    numero_tarjeta = models.TextField()  # This field type is a guess.
+    cvv = models.TextField()  # This field type is a guess.
+    fecha_otorgamiento = models.TextField()  # This field type is a guess.
+    fecha_expiracion = models.TextField()  # This field type is a guess.
+    marca_tarjetaid = models.TextField(db_column='marca_tarjetaId')  # Field name made lowercase. This field type is a guess.
+    tipo_clienteid = models.TextField(db_column='tipo_clienteId')  # Field name made lowercase. This field type is a guess.
+    tipo_cuentaid = models.TextField(db_column='tipo_cuentaId')  # Field name made lowercase. This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'cards'
+
+
+class Cliente(models.Model):
+    customer_id = models.AutoField()
+    customer_name = models.TextField()
+    customer_surname = models.TextField()  # This field type is a guess.
+    customer_dni = models.TextField(db_column='customer_DNI')  # Field name made lowercase.
+    dob = models.TextField(blank=True, null=True)
+    branch_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'cliente'
+
+
+class Cuenta(models.Model):
+    account_id = models.AutoField()
+    customer_id = models.IntegerField()
+    balance = models.IntegerField()
+    iban = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'cuenta'
+
 
 class Empleado(models.Model):
     employee_id = models.AutoField()
@@ -15,13 +56,21 @@ class Empleado(models.Model):
     employee_dni = models.TextField(db_column='employee_DNI')  # Field name made lowercase.
     branch_id = models.IntegerField()
 
-    def __str__(self) -> str:
-        return self.name
-
     class Meta:
         managed = False
         db_table = 'empleado'
 
+
+class Prestamo(models.Model):
+    loan_id = models.AutoField()
+    loan_type = models.TextField()
+    loan_date = models.TextField()
+    loan_total = models.IntegerField()
+    customer_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'prestamo'
 
 
 class Sucursal(models.Model):
@@ -30,9 +79,12 @@ class Sucursal(models.Model):
     branch_name = models.TextField()
     branch_address_id = models.IntegerField()
 
-    def __str__(self) -> str:
-        return self.name
-
     class Meta:
         managed = False
         db_table = 'sucursal'
+
+
+
+class Usuario(models.Model):  
+    user = models.OneToOneField(User)
+    customer_dni = models.ForeignKey(Cliente, on_delete=models.CASCADE)
