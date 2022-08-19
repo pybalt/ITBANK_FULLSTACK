fbb6a37cc9ba4468f3fedf993b5c3ce9ad166a93
LautyT02 <lautyt02@gmail.com>
2022-08-19 18:50:47 -0300

    Formulario Préstamos

--- homebanking/prestamos/views.py
+++ homebanking/prestamos/views.py
@@ -46,7 +46,7 @@ def prestamos(request):
             nameRecived,surnameRecived,dniRecived):
                 print('Coinciden datos de usuario con los de cliente')
                 #Extracción del cliente a partie de nombre, apellido y dni
-                cliente_filtrado = Cliente.objects.filter(customer_name=nameRecived).filter(customer_surname=surnameRecived).filter(customer_dni= dniRecived)#.using(alias='itbank')
+                cliente_filtrado = Cliente.objects.filter(customer_name=nameRecived).filter(customer_surname=surnameRecived).filter(customer_dni= dniRecived)
                 print('Cliente Filtrado:')
                 #print(cliente_filtrado)
                 #Obtención del Customer ID
@@ -54,19 +54,19 @@ def prestamos(request):
                 print('customer_idRecived:')
                 print(customer_idRecived)
                 #Obtención del Tipo de Cliente
-                tarjeta = Cards.objects.filter(customer_id = customer_idRecived)#.using(alias='itbank')
+                tarjeta = Cards.objects.filter(customer_id = customer_idRecived)
                 tipoClienteId = tarjeta.get(customer_id = customer_idRecived).tipo_clienteid
                 print(tipoClienteId)
-                tipoDeClienteQS = Tipocliente.objects.filter(tipo_clienteid = tipoClienteId)#.using(alias='itbank')
+                tipoDeClienteQS = Tipocliente.objects.filter(tipo_clienteid = tipoClienteId)
                 tipoDeCliente =tipoDeClienteQS.get(tipo_clienteid = tipoClienteId).tipo_cliente
                 print(tipoDeCliente)
                 #Comprobación Monto dentro del límite
                 if chequearMonto(loan_totalRecived, tipoDeCliente):
                     #Se cargan los datos a la tabla de prestamos
                     Prestamo(loan_type=loan_typeRecived, loan_date=loan_dateRecived,
-                    loan_total=loan_totalRecived, customer_id =customer_idRecived).save()#using='itbank')
+                    loan_total=loan_totalRecived, customer_id =customer_idRecived).save()
                     #Se modifica el campo balance de la tabla de Cuenta
-                    cuentasQS=Cuenta.objects.all()#.using(alias='itbank')
+                    cuentasQS=Cuenta.objects.all()
                     #cuentaAmodificar = Cuenta.objects.filter(customer_id=customer_idRecived)
                     balancePrevio= cuentasQS.get(customer_id=customer_idRecived).balance
                     print('balance previo')
