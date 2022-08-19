0d6e3a30033ba1cc19b1d8754ea29198436dce26
pybalt <leobravolbb2002bej@gmail.com>
2022-08-19 19:11:21 -0300

    logout implement

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = 'login/'
+LOGOUT_REDIRECT_URL = 'login'
 
 #AUTH_USER_MODEL = 'webitbank.Usuario'
 
