00bed9569817eac9d03a83b67010143b746a0df0
LautyT02 <lautyt02@gmail.com>
2022-08-29 22:19:13 -0300

    Formulario de Prestamos Funcionando
    Falta eliminar prints de control, código comentado y corregir CSS

--- homebanking/prestamos/forms.py
+++ homebanking/prestamos/forms.py
@@ -5,23 +5,12 @@ from django import forms
 from .models import Prestamo
 from cuentas.models import Cuenta
 from .choices import tipo_prestamo as tipo
-"""
+from django.contrib.admin import widgets 
+
 class SolicitudPrestamoForm(forms.ModelForm):
     name = forms.CharField(max_length=50, label= 'Nombres', required=True)
     surname = forms.CharField(max_length=50, label= 'Apellidos', required=True)
     dni= forms.IntegerField(label= 'DNI', required= True)
-    account= forms.ChoiceField(queryset )
     class Meta:
         model=Prestamo
-        fields = ['loan_type','loan_date','loan_total']
-"""
-class SolicitudPrestamoForm(forms.Form):
-    name = forms.CharField(max_length=50, label= 'Nombres', required=True)
-    surname = forms.CharField(max_length=50, label= 'Apellidos', required=True)
-    dni= forms.IntegerField(label= 'DNI', required= True)
-    #account= forms.ChoiceField(queryset )
-    startDate = forms.DateField(label = 'Fecha de Inicio', required=True, widget=forms.SelectDateWidget(format="%d %b %Y"))
-    loan_type = forms.ChoiceField(label = 'Tipo de Préstamo', required=True, choices= tipo)
-    amount = forms.IntegerField(label = 'Monto del Préstamo', required=True)
-    #forms.SelectDateWidget()
-
+        fields = ['name','surname','dni','loan_date','loan_type','loan_total']#
--- homebanking/prestamos/views.py
+++ homebanking/prestamos/views.py
@@ -8,7 +8,19 @@ from cuentas.models import Cuenta
 from tarjetas.models import Cards
 from django.contrib.auth.models import User 
 from movimiento.models import Movimientos
-import datetime
+from datetime import datetime
+"""
+
+
+
+"Se recibe año, mes, dia"
+dato_recibido = "2016-04-14"
+
+dato_recibido = datetime.strptime(dato_recibido, "%Y-%m-%d") #Crea un objeto con el formato especificado
+
+dato_recibido = datetime.strftime(dato_recibido, "%d-%m-%Y")
+
+print(dato_recibido, type(dato_recibido))"""
 
 def chequearUsuarioFormulario(nombreUsuario, 
                               apellidoUsuario, 
@@ -29,6 +41,7 @@ def chequearMonto(monto,tipo_Cliente):
 def prestamosView(request):
     print('hola1')
     form_prestamo = SolicitudPrestamoForm
+    mensaje="""Formato correcto de Fecha: día/mes/Año Completo"""
     if request.method == "POST":
         form_prestamo=SolicitudPrestamoForm(data = request.POST)
         if form_prestamo.is_valid():
@@ -36,21 +49,29 @@ def prestamosView(request):
             try:
                 cliente=Cliente.objects.get(user=request.user)
             except Cliente.DoesNotExist:
-                return redirect(reverse('prestamos')+"?nocliente")
+                mensaje="""el usuario no tiene ningún cliente asociado"""
+                #return redirect(reverse('prestamos')+"?nocliente")
             nameUser = cliente.customer_name
             surnameUser = cliente.customer_surname
             dniUser = str(cliente.customer_dni)
             print(nameUser)
             print(surnameUser)
-            print(dniUser)
+            print(type(dniUser))
             #Datos a comparar sacados del Formulario
             nameRecived = request.POST.get('name','')
             surnameRecived = request.POST.get('surname','')
             dniRecived = request.POST.get('dni','')
             #Otros Datos del Formulario
-            loan_dateRecived = request.POST.get('startDate','')
+            loan_dateRecived = request.POST.get('loan_date','')
+            try:
+                loan_dateRecived = datetime.strptime(loan_dateRecived, "%d/%m/%Y")
+                print(loan_dateRecived)
+                loan_dateRecived = datetime.strftime(loan_dateRecived,'%Y-%m-%d')
+            except:
+                mensaje= """Formato de fecha Incorrecto. """ +mensaje
+                #return redirect(reverse('prestamos')+"?formatofecha")
             loan_typeRecived = request.POST.get('loan_type','')
-            loan_totalRecived = int(request.POST.get('amount',''))
+            loan_totalRecived = int(request.POST.get('loan_total',''))
             #Comprobar si coincide formulario con Sesión
             if chequearUsuarioFormulario(nameUser,surnameUser,dniUser,
             nameRecived,surnameRecived,dniRecived):
@@ -64,18 +85,27 @@ def prestamosView(request):
                     try:
                         cuenta = Cuenta.objects.filter(tipo_cuenta= 'CAP').get(customer=cliente)
                     except Cliente.DoesNotExist:
-                        return redirect(reverse('prestamos')+"?nocuenta")
+                        mensaje = """Usted no posee una caja de ahorro en pesos"""
+                        #return redirect(reverse('prestamos')+"?nocuenta")
                     prestamo=Prestamo(loan_type=loan_typeRecived, loan_date=loan_dateRecived,
                     loan_total=loan_totalRecived, account =cuenta, estado = 'APB')
-                    prestamo.save()
+                    
+                    try:
+                        prestamo.save() 
+                    except:
+                        mensaje="""Error al registrar el préstamo."""
+                        #return redirect(reverse('prestamos')+"?noprestamo")
                     #Se modifica el campo balance de la tabla de Cuenta
                     print('balance previo')
                     print(cuenta.balance)
-                    cuenta.update(balance=F('balance')+loan_totalRecived)
+                    cuenta.balance = cuenta.balance+loan_totalRecived
+                    cuenta.save(update_fields=['balance'])
                     print('balance nuevo')
                     print(cuenta.balance)
                     #Se registra el movimiento
                     cuenta_banco= Cuenta.objects.get(account_id = 1)
+                    cuenta_banco.balance=cuenta_banco.balance-loan_totalRecived
+                    cuenta_banco.save(update_fields=['balance'])
                     movimiento = Movimientos(
                             monto = loan_totalRecived,
                             cuenta_remitente = cuenta_banco,
@@ -83,13 +113,20 @@ def prestamosView(request):
                             tipo_movimiento = 'PR'
                             )
                     movimiento.save()
-                    return redirect(reverse('prestamos')+ "?ok")
+                    mensaje="""Su prestamo ha sido aceptado, el saldo de su cuenta ya fue actualizado"""
+                    #return redirect(reverse('prestamos')+ "?ok")
                 else:
-                    return redirect(reverse('prestamos')+ "?nomonto") 
-            
-            return redirect(reverse('prestamos')+"?nocoincide")  
+                    mensaje="""El monto solicitado supera el límite establecido para su cuenta.
+            Los límites son: 500000 para clientes Black, 300000 para clientes Gold y 10000 para clientes Classic"""
+                    #return redirect(reverse('prestamos')+ "?nomonto") 
+            else:
+                mensaje="""Los datos ingresados en el formulario no coincide con los asociados al usuario logueado. 
+            Recuerde que solo puede solicitar el prestamo desde el usuario asociado a la cuenta en la que desea recibir el dinero"""
+                #return redirect(reverse('prestamos')+"?nocoincide")  
         
-        return redirect(reverse('prestamos')+ "?notok")    
-    
-    return render(request, "webitbank/pages/prestamos.html",
-    {'form':form_prestamo})
+        else:
+            mensaje= """lguno de los campos ingresados no es válido"""
+            #return redirect(reverse('prestamos')+ "?notok")    
+    diccionario = {'form':form_prestamo,'msj':mensaje}
+    return render(request, "prestamos/prestamos.html",
+    diccionario)
