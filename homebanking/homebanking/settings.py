e5c94d420133bd097c962b02227390df183c88a8
pybalt <leobravolbb2002bej@gmail.com>
2022-08-30 13:19:42 -0300

    fix login/logout redirects

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_URL = 'accounts/login'
-LOGOUT_URL = 'accounts/login'
+LOGIN_URL = '/accounts/login'
+LOGOUT_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
